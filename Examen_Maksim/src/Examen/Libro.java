2a7fc3d3706adac83469ff11de4c42b76eb74bf3
muncu <muncu@educacion.navarra.es>
2025-12-17 10:32:53 +0100

    Constructor añadido

--- Examen_Maksim/src/Examen/Libro.java
+++ Examen_Maksim/src/Examen/Libro.java
@@ -3,4 +3,11 @@ package Examen;
 public class Libro {
     private String  titulo, autor, isbn;
     private boolean prestado;
+
+    public Libro(String titulo, String autor, String isbn, boolean prestado) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.prestado = prestado;
+    }
 }
