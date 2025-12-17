53bb8a77992146a2aa33273f0e42153b98f6e1df
muncu <muncu@educacion.navarra.es>
2025-12-17 10:50:24 +0100

    Añadido código en Libreria y eliminado clase main autogenerada

--- Examen_Maksim/src/Examen/Libreria.java
+++ Examen_Maksim/src/Examen/Libreria.java
@@ -1,4 +1,8 @@
 package Examen;
 
 public class Libreria {
+    public static void main(String[] args) {
+        Libro libro1 = new Libro("Examen", "TuNombre" ,"171225" ,false);
+        System.out.println(libro1.toString());
+    }
 }
